5242c036b0997ea7f77d0d36d41e10472383745a
Gilmar de Alcantara <gilmar.alcantara@ibm.com>
2019-02-24 12:26:00 -0300

    Add gulp concat, html-replace and default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp')
   ,imagemin = require('gulp-imagemin')
-  ,clean = require('gulp-clean');
+  ,clean = require('gulp-clean')
+  ,concat = require('gulp-concat')
+  ,htmlReplace = require('gulp-html-replace');
 
 // removida a dependência de build-img
 gulp.task('copy', ['clean'], function() {
@@ -14,9 +16,31 @@ gulp.task('clean', function() {
 });
 
 // adicionando a dependência copy
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
 
   gulp.src('dist/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 });
+
+gulp.task('build-js', function() {
+    gulp.src(['dist/js/jquery.js', 
+      'dist/js/home.js', 
+      'dist/js/ativa-filtro.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build-html', function() {
+
+    gulp.src('dist/**/*.html')
+        .pipe(htmlReplace({
+            'js': 'js/all.js'
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['copy'], function() {
+    gulp.start('build-img', 'build-html', 'build-js');
+});
+
